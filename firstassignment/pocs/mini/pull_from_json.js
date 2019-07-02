ac1bf5536597d64a50f639749d52529d8b5d999c
Trey Hand <hand.trey@gmail.com>
2019-07-02 08:27:30 -0500

    First Assignment
    Project 1 - MVC
    Project 2 - Quiz game

---
+++ firstassignment/pocs/mini/pull_from_json.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", init, false);
+
+function init() {
+
+  //Ajax functionality from browser via javascript
+  var xhr = new XMLHttpRequest();
+  //check for successful file loading (status == 200) to execute callback
+  xhr.successCallBack = function {
+    var obj = JSON.parse(xhr.responseText);
+    var 
+  }
+
+}
